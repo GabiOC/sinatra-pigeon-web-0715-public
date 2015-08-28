ccacc6169a930b8e026f7a8a11644bcd44790a53
GabiOC <oconnor.gabriele@gmail.com>
2015-08-28 16:25:37 -0400

    all done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.join(root, "../views/") }
+
+  helpers do
+    def link_for(object)
+      resource_url = "/" + object.class.name.downcase + "s"
+      id = object.id
+      name = object.name
+      %{<a href="#{resource_url}/#{id}">#{name}</a>}
+    end
+  end
 end
--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,2 +1,40 @@
 # create your PigeonsController here
 # it should inherit from ApplicationController
+require 'pry'
+
+class PigeonsController < ApplicationController
+  get '/pigeons' do
+    @pigeons = Pigeon.all
+    erb :'pigeons/index'
+  end
+
+  get '/pigeons/new' do
+    erb :'pigeons/new'
+  end
+
+  get "/pigeons/:id" do
+    @pigeon = Pigeon.find_by(params[:id])
+    erb :'pigeons/show'
+  end
+
+  get "/pigeons/:id/edit" do
+    @pigeon = Pigeon.find_by(params[:id])
+    erb :'pigeons/edit'
+  end
+
+  patch "/pigeons/:id" do
+    p = Pigeon.find_by(params[:id])
+    p.update(name: params[:name], color: params[:color], lives: params[:lives], gender: params[:gender])
+    redirect to("/pigeons/#{params[:id]}")
+  end
+
+  post '/pigeons' do
+    p = Pigeon.create(params[:pigeon])
+    redirect to('/pigeons')
+  end
+
+  delete "/pigeons/:id" do
+    Pigeon.delete(params[:id])
+    redirect to('/pigeons')
+  end
+end
